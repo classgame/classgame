be7396a81f3b0c433c2788598513d09fffb3e15b
Valter <valterinside@gmail.com>
2016-09-25 23:08:21 -0300

    create new course with select of category and registry this course with your select category, list courses by category selected
    Signed-off-by: Valter <valterinside@gmail.com>

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -14,6 +14,7 @@ class CoursesController < ApplicationController
 
   # GET /courses/new
   def new
+    @category = Category.all
     @course = Course.new
   end
 
@@ -69,6 +70,6 @@ class CoursesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def course_params
-      params.require(:course).permit(:title, :description, :nivel, :workload, :active, :completed_edition)
+      params.require(:course).permit(:title, :description, :nivel, :workload, :active, :completed_edition, :category_id)
     end
 end
