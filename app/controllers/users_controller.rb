176e0577720c748118fa20799b03ecfa1b6150ca
paulojp <jprox000@gmail.com>
2016-10-11 04:11:30 -0300

    created general rank from users; filtered by the highest level to the lowest

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.includes(:performace).order("performaces.nivel desc").limit(10)
   end
 
   # GET /users/1
