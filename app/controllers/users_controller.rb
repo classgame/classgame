7ad3faa16339115bb38a7d60b418bf531b628f24
paulojp <jprox000@gmail.com>
2016-10-11 02:41:15 -0300

    edited user views:index, show, application and edit; removed action delet from user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,16 +51,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
-  def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
