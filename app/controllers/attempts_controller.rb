fdae93bd7bf9cdc734946def495ae0d5403b0d47
Valter <valterinside@gmail.com>
2016-10-26 17:43:10 -0300

    refactor controller attempts_controller action create and new
    Signed-off-by: Valter <valterinside@gmail.com>

--- app/controllers/attempts_controller.rb
+++ app/controllers/attempts_controller.rb
@@ -1,96 +1,47 @@
 class AttemptsController < ApplicationController
   before_action :set_attempt, only: [:show, :edit, :update, :destroy]
 
-  # GET /attempts
-  # GET /attempts.json
   def index
     @attempts = Attempt.all
   end
 
-  # GET /attempts/1
-  # GET /attempts/1.json
   def show
   end
 
-  # GET /attempts/new
   def new
-    @chapter     = Chapter.find(params[:id])
-    @@chapter    = @chapter
-    @@contents ||= @chapter.all_contents
-    @contents_t  = @chapter.all_contents
-    
-    if @@contents.first
-      @partial ||= @@contents.first.class.table_name
-      
-      if @@contents.first.class.table_name == 'exercises'
-        @exercise = @@contents.first
-      elsif @@contents.first.class.table_name == 'texts'
-        @text = @@contents.first
-      else
-        @video = @@contents.first
-      end
+    if params[:id]
+      @@chapter  = Chapter.find(params[:id])
+      @@contents = @@chapter.all_contents
     end
-
-    if @@contents.shift.nil?
-      @@contents = nil
-      redirect_to course_path(@chapter.course), notice: "Parabéns você terminou o chapter #{@chapter.title}!"
-    end
-  
+    @contents  = @@contents 
   end
 
-  # GET /attempts/1/edit
   def edit
   end
 
-  # POST /attempts
-  # POST /attempts.json
   def create
+    @attempt = Attempt.new()
     if params[:question]
-      @attempt = Attempt.new()
-      @attempt.final_experience_question(Alternative.where(id: attempt_params),current_user)
-
-      respond_to do |format|
-        if @attempt.save
-          format.html { redirect_to new_attempt_path(@@chapter), notice: "Parabéns você ganhou #{@attempt.experience}xp" }
-          format.json { render :show, status: :created, location: courses_path }
-        else
-          format.html { render :new }
-          format.json { render json: @attempt.errors, status: :unprocessable_entity }
-        end
-      end 
-    
+      @attempt.final_experience_question(Alternative.where(id: attempt_params), current_user)
     elsif params[:text]
-      @attempt = Attempt.new()
       @attempt.final_experience_text(Text.find(params[:text][:id]), current_user)
-      
-      respond_to do |format|
-        if @attempt.save
-          format.html { redirect_to new_attempt_path(@@chapter), notice: "Parabéns você ganhou #{@attempt.experience}xp" }
-          format.json { render :show, status: :created, location: courses_path }
-        else
-          format.html { render :new }
-          format.json { render json: @attempt.errors, status: :unprocessable_entity }
-        end
-      end
-    
     else
-      @attempt = Attempt.new()
       @attempt.final_experience_video(Video.find(params[:video][:id]), current_user)
-
-      respond_to do |format|
-        if @attempt.save
-          format.html { redirect_to new_attempt_path(@@chapter), notice: "Parabéns você ganhou #{@attempt.experience}xp" }
-          format.json { render :show, status: :created, location: courses_path }
-        else
-          format.html { render :new }
-          format.json { render json: @attempt.errors, status: :unprocessable_entity }
-        end
-      end  
     end  
-  end
 
-  # PATCH/PUT /attempts/1
-  # PATCH/PUT /attempts/1.json
+    if @attempt.save
+      @@contents.shift
+      if @@contents == []
+        @@contents = nil
+        redirect_to course_path(@@chapter.course), notice: "Parabéns você terminou o chapter #{@@chapter.title}!"
+      else
+        respond_to do |format|
+          format.html {redirect_to new_create_attempt_path, notice: "Parabéns você ganhou #{@attempt.experience}xp"}
+        end 
+      end
+    end
+  end
+    
   def update
     respond_to do |format|
       if @attempt.update(attempt_params)
@@ -103,8 +54,6 @@ class AttemptsController < ApplicationController
     end
   end
 
-  # DELETE /attempts/1
-  # DELETE /attempts/1.json
   def destroy
     @attempt.destroy
     respond_to do |format|
@@ -114,12 +63,10 @@ class AttemptsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_attempt
       @attempt = Attempt.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def attempt_params
       if params[:question]
         params[:question][:alternative_attributes] = params[:question][:alternative_attributes].map {|attrs| attrs.last } 
